75be036c48e41b311d3ef53699079b45450d0e73
Joseph <yeojunjeon42@gmail.com>
2025-10-08 01:43:21 +0900

    GitHub actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ensoltech-website/',
   server: {
     port: 3000,
     watch: {
